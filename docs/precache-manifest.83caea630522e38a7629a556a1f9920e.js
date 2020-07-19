dbdbb1458e2933bd8aec53fe3f48cef57eb764ad
GaGo Aharonyan <gago9639@gmail.com>
2020-07-19 21:40:12 +0400

    docs

---
+++ docs/precache-manifest.83caea630522e38a7629a556a1f9920e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb567fda61e20f619c31983304478071",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f53ba46d8fd37b64165",
+    "url": "/static/css/main.7a2f8025.chunk.css"
+  },
+  {
+    "revision": "87a3bf32db6be40140f2",
+    "url": "/static/js/2.05fd87db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.05fd87db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f53ba46d8fd37b64165",
+    "url": "/static/js/main.569da3b8.chunk.js"
+  },
+  {
+    "revision": "9cb5c655baca543931ab",
+    "url": "/static/js/runtime-main.39359f0a.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom';
 import './main.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router, HashRouter} from 'react-router-dom';
 import store from './redux/store';
 import {Provider} from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}> 
-      <Router>
+    <HashRouter basename="/">
         <App />
-      </Router>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
